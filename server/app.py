153040d5588affeb6c96e0152b4c4046bf9093b7
Sung-Jin Ahn <sahn0307@gmail.com>
2024-04-06 18:02:54 -0600

    all postman requests work

--- server/app.py
+++ server/app.py
@@ -18,12 +18,61 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+class Restaurants(Resource):
+    def get(self):
+        try:
+            restaurants = [restaurant.to_dict() for restaurant in Restaurant.query]
+            return restaurants, 200
+        except Exception as e:
+            return {"message": str(e)}, 404
+        
+api.add_resource(Restaurants, "/restaurants")
 
+class RestaurantsById(Resource):
+    def get(self, id):
+        try:
+            if restaurant := db.session.get(Restaurant, id):
+                return restaurant.to_dict(only=("id", "name", "address", "restaurant_pizzas")), 200
+            else:
+                return {"error": "Restaurant not found"}, 404
+        except Exception as e:
+            return {"error": str(e)}, 400
 
-@app.route("/")
-def index():
-    return "<h1>Code challenge</h1>"
+    def delete(self, id):
+        try:
+            if restaurant := db.session.get(Restaurant, id):
+                db.session.delete(restaurant)
+                db.session.commit()
+                return {"message": ""}, 204
+            else:
+                return {"error": "Restaurant not found"}, 404
+        except Exception as e:
+            return {"error": str(e)}, 400
+api.add_resource(RestaurantsById, "/restaurants/<int:id>")
 
+class Pizzas(Resource):
+    def get(self):
+        try:
+            pizzas = [pizza.to_dict(only=("id", "ingredients", "name")) for pizza in Pizza.query]
+            return pizzas, 200
+        except Exception as e:
+            return {"message": str(e)}, 400
+        
+api.add_resource(Pizzas, "/pizzas")
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        try:
+            data = request.json
+            new_pizza = RestaurantPizza(**data)
+            db.session.add(new_pizza)
+            db.session.commit()
+    
+            return new_pizza.to_dict(rules=()), 201
+        except Exception as e:
+            db.session.rollback()
+            return {"errors": ["validation errors"]}, 400
+api.add_resource(RestaurantPizzas, "/restaurant_pizzas")
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
